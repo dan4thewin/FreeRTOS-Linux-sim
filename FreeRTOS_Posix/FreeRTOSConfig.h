5f9a1e017c7bd2dabab39c7571cf1d56ba9e3a8a
Dan Good <gooddan@amazon.com>
2019-12-20 03:54:43 +0000

    Convert spaces to tabs

--- FreeRTOS_Posix/FreeRTOSConfig.h
+++ FreeRTOS_Posix/FreeRTOSConfig.h
@@ -71,8 +71,8 @@
 #define configMINIMAL_STACK_SIZE		( ( unsigned portSHORT ) 4 ) /* This can be made smaller if required. */
 #define configTOTAL_HEAP_SIZE			( ( size_t ) ( 32 * 1024 ) )
 #define configMAX_TASK_NAME_LEN			( 16 )
-#define configUSE_TRACE_FACILITY    	1
-#define configUSE_16_BIT_TICKS      	0
+#define configUSE_TRACE_FACILITY		1
+#define configUSE_16_BIT_TICKS			0
 #define configIDLE_SHOULD_YIELD			1
 #define configUSE_CO_ROUTINES 			1
 #define configUSE_MUTEXES				1
@@ -90,11 +90,11 @@
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
 
-#define INCLUDE_vTaskPrioritySet        	1
-#define INCLUDE_uxTaskPriorityGet       	1
-#define INCLUDE_vTaskDelete             	1
+#define INCLUDE_vTaskPrioritySet			1
+#define INCLUDE_uxTaskPriorityGet			1
+#define INCLUDE_vTaskDelete					1
 #define INCLUDE_vTaskCleanUpResources   	1
-#define INCLUDE_vTaskSuspend            	1
+#define INCLUDE_vTaskSuspend				1
 #define INCLUDE_vTaskDelayUntil				1
 #define INCLUDE_vTaskDelay					1
 #define INCLUDE_uxTaskGetStackHighWaterMark 0 /* Do not use this option on the PC port. */
